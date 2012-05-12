3c554cd0f58522631ac48f9dee703a1a60fe71b8
B V Satyaram <bvsatyaram@gmail.com>
2012-05-12 17:23:30 +0530

    Initialising Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpenseTrackerDummy::Application.config.secret_token = '6e235500ad08762236dbee29019620abe22a8338a4d04f66127d5cf39b57838a911dc23f6f65623b04f07d3665f045ba2b1e15bbf01cc65687bf1762a667c22c'
